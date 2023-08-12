f61b5067c7236192e028610f1e09c3833865b20c
Samar Singh <singhsamar2002@gmail.com>
2023-08-12 18:55:45 +0530

    api integrated

---
+++ lib/Screens/Providers/homeProvider.dart
@@ -0,0 +1,33 @@
+import 'package:bookflix/Constants.dart';
+import 'package:bookflix/Service/apiservice.dart';
+import 'package:flutter/material.dart';
+
+import '../../Model/Books.dart';
+
+class HomeNotifer extends ChangeNotifier {
+  List<Item>? mangaBook;
+  ApiService api = ApiService();
+  Future<List<Item>> _fetchBook(
+    String type,
+    String printType,
+    String order,
+  ) async {
+    String url = "";
+    url = baseUrl +
+        "q=${type}&printType=${printType}&orderBy=${order}&key=${key}";
+    return await api.fetchBooks(url);
+  }
+
+  fetchManga() async {
+    mangaBook = await _fetchBook('manga', 'book', 'relevance');
+    notifyListeners();
+  }
+
+  fetchfiction() {
+    _fetchBook('fiction', 'book', 'relevance');
+  }
+
+  fetchNonfiction() {
+    _fetchBook('nonfiction', 'book', 'relevance');
+  }
+}
---
+++ lib/Service/apiservice.dart
@@ -0,0 +1,46 @@
+import 'package:bookflix/Constants.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../Model/Books.dart';
+
+class ApiService {
+  Future<List<Item>> fetchBooks(String url) async {
+    try {
+      print('Making api call');
+      final response = await http.get(Uri.parse(url));
+      if (response.body != null) {
+        if (response.statusCode == 200) {
+          print('Api call successful');
+
+          final Map<String, dynamic> jsonData = json.decode(response.body);
+          // print(response.body);
+          // print(Bookfetch.fromJson(jsonData).items);
+
+          final Bookfetch bookfetch = Bookfetch.fromJson(jsonData);
+          // if (jsonData.containsKey('items')) {
+          //   try {
+          //     setState(() {
+          //       popularBooks = Bookfetch.fromJson(jsonData).items;
+          //     });
+          //   } catch (e) {
+          //     print('error is :${e}');
+          //   }
+          // }
+
+          //  print(popularBooks);
+          return bookfetch.items;
+        } else {
+          print('call failed with status code ${response.statusCode}');
+          return [];
+        }
+      } else {
+        print('response body is null');
+        return [];
+      }
+    } catch (e) {
+      print('error is :${e}');
+      return [];
+    }
+  }
+}
